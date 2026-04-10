a40342180e82733ec361f84ed9b4e9fe821ad902
Areeb <muhammadareeb334@gmail.com>
2026-04-10 17:00:39 +0500

    Made Google Auth API and Integrated it to frontend

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,6 +1,9 @@
 import User from "../model/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { OAuth2Client } from "google-auth-library";
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const generateToken = (user) => {
   return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
@@ -57,6 +60,13 @@ export const loginUser = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
+    if (!user.password) {
+      return res.status(400).json({
+        message:
+          "This account was created using Google. Please log in with Google.",
+      });
+    }
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ message: "Invalid credentials" });
@@ -73,5 +83,44 @@ export const loginUser = async (req, res) => {
     });
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const googleAuth = async (req, res) => {
+  // console.log("Google Auth Route Hit"); 
+  try {
+    const { token } = req.body;
+
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const payload = ticket.getPayload();
+    const { email, name } = payload;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({
+        name,
+        email,
+        password: null,
+      });
+    }
+    const responseData = {
+      token: generateToken(user),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    };
+    // console.log("Sending to CLient: ", responseData);
+    return res.status(200).json(responseData);
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: "Google auth failed" });
   }
 };
